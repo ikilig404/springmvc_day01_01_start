582ba8326dfe5006b87aae864cd890798a3c87ae
Wjer <17909992954@qq.com>
2019-09-09 19:55:55 +0800

    参数绑定

---
+++ src/main/java/cn/itcast/controller/ParamController.java
@@ -0,0 +1,23 @@
+package cn.itcast.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 请求参数绑定
+ */
+@Controller
+@RequestMapping("/param")
+public class ParamController {
+
+    /**
+     * 请求参数绑定入门
+     * @return
+     */
+    @RequestMapping("/testParam")
+    public String testParam(String username) {
+        System.out.println("执行了...");
+        System.out.println("用户名：" + username);
+        return "success";
+    }
+}
